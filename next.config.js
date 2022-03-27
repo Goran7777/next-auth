bbc49d17a7b726b85482de64837d6765a47f468b
Goran7777 <goranilicilke893@gmail.com>
2022-03-27 12:58:55 +0200

    first commit

---
+++ next.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const withSass = require('@zeit/next-sass');
+module.exports = {
+  reactStrictMode: true,
+};
+
+module.exports = withSass({
+  cssModules: true,
+});
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+module.exports = {
+  future: {
+    webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
+    // Looks like backward compatibility approach.
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
+
+    return config;
+  },
+};
